41c4dc41eba64a6d4ae0953dc1a2ae0f695f7f61
Marina Menghetti <gautier_dervaux@hotmail.com>
2020-06-18 17:11:18 +0200

    ex of actions and mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { INCREMENT } from "./mutation-types";
 
 Vue.use(Vuex);
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    count: 0,
+  },
+  mutations: {
+    [INCREMENT](state, amount) {
+      state.count += amount ? amount : 1;
+    },
+  },
+  actions: {
+    incrementCount(context, amount) {
+      context.commit("INCREMENT", amount);
+    },
+  },
+  modules: {},
 });
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,18 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <UserProfile :id="$route.params.id" />
+    <p>{{ count }}</p>
+    <button @click="(ev) => incrementCount()">Click to increment</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import UserProfile from "@/components/UserProfile.vue";
-
+import { mapState, mapActions } from "vuex";
 export default {
   name: "Profile",
   components: {
-    UserProfile
-  }
+    UserProfile,
+  },
+  computed: {
+    ...mapState(["count"]),
+  },
+  methods: {
+    ...mapActions(["incrementCount"]),
+  },
+  beforeMount() {
+    this.incrementCount(2);
+  },
 };
 </script>
 Z
