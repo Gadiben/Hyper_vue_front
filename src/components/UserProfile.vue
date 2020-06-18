d6a444d2527bbd751d3cbdb0a35ca816c0b84402
Marina Menghetti <gautier_dervaux@hotmail.com>
2020-06-18 16:15:31 +0200

    route to profile with id

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -9,7 +9,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class UserProfile extends Vue {
-  @Prop() private id!: Integer;
+  @Prop() private id!: number;
 }
 </script>
 
---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <UserProfile :id="$route.params.id" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserProfile from "@/components/UserProfile.vue";
+
+export default {
+  name: "Profile",
+  components: {
+    UserProfile,
+  },
+};
+</script>
+Z
