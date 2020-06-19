8db5145ca53ac4ad52f8ef039f9e9280af2bbd92
Marina Menghetti <gautier_dervaux@hotmail.com>
2020-06-19 16:43:41 +0200

    auth endpoint & refacto

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import usersModule from "./modules/usersModule";
+import authModule from "./modules/authModule";
 Vue.use(Vuex);
 const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
   modules: {
     usersModule,
-  },
-  state: {
-    apiURL: "",
+    authModule,
   },
 });
